636e82b08335a557033dafb3326cfdaffaadbe3d
choimory <choimory@naver.com>
2024-02-08 00:08:54 +0900

    system: password encoder bean

--- src/main/kotlin/com/choimory/kotlinmemberapi/config/SecurityConfig.kt
+++ src/main/kotlin/com/choimory/kotlinmemberapi/config/SecurityConfig.kt
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import kotlin.jvm.Throws
 
 @EnableWebSecurity
 @Configuration
@@ -18,4 +18,7 @@ class SecurityConfig {
 
         return http.build()
     }
+
+    @Bean
+    fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
 }
