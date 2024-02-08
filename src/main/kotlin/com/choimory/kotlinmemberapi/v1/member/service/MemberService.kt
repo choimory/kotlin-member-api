d1f75947dbe202489b8e3ef479ef8d274792b77a
choimory <choimory@naver.com>
2024-02-08 10:10:00 +0900

    feature: open in view false 및 읽기전용 트랜잭션 설정

--- src/main/kotlin/com/choimory/kotlinmemberapi/v1/member/service/MemberService.kt
+++ src/main/kotlin/com/choimory/kotlinmemberapi/v1/member/service/MemberService.kt
@@ -1,7 +1,9 @@
 package com.choimory.kotlinmemberapi.v1.member.service
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class MemberService {
 }
