78c3c547c3d174cd89a923e189e5badb8b36e7ec
choimory <choimory@naver.com>
2024-02-08 10:10:26 +0900

    feature: 공용 Audit 엔티티 설계

--- src/main/kotlin/com/choimory/kotlinmemberapi/common/domain/entity/CommonDateTimeAt.kt
+++ src/main/kotlin/com/choimory/kotlinmemberapi/common/domain/entity/CommonDateTimeAt.kt
@@ -1,4 +1,21 @@
 package com.choimory.kotlinmemberapi.common.domain.entity
 
-class CommonDateTimeAt {
-}
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.io.Serializable
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class CommonDateTimeAt(
+    @CreatedDate
+    protected var createdAt:LocalDateTime = LocalDateTime.now(),
+
+    @LastModifiedDate
+    protected var modifiedAt:LocalDateTime? = null,
+
+    protected var deletedAt:LocalDateTime? = null,
+) :Serializable
