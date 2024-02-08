c23ddef5ae6690f01420e20b485dd0b80d967c09
choimory <choimory@naver.com>
2024-02-08 14:41:42 +0900

    feature: 기본 엔티티 설계

--- src/main/kotlin/com/choimory/kotlinmemberapi/v1/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/choimory/kotlinmemberapi/v1/member/repository/MemberRepository.kt
@@ -1,4 +1,9 @@
 package com.choimory.kotlinmemberapi.v1.member.repository
 
-interface MemberRepository {
+import com.choimory.kotlinmemberapi.v1.member.domain.entity.Member
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MemberRepository : JpaRepository<Member, Long> {
 }
--- src/main/kotlin/com/choimory/kotlinmemberapi/v1/member/service/MemberService.kt
+++ src/main/kotlin/com/choimory/kotlinmemberapi/v1/member/service/MemberService.kt
@@ -1,9 +1,12 @@
 package com.choimory.kotlinmemberapi.v1.member.service
 
+import com.choimory.kotlinmemberapi.v1.member.repository.MemberRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional(readOnly = true)
-class MemberService {
+class MemberService(
+    private val memberRepository: MemberRepository
+) {
 }
